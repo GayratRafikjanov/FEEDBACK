b17dab14119e64d9c3713a0febac7e54fdc67752
GRafikjanov <gayratrafikjanov@gmail.com>
2025-03-12 16:28:53 +0500

    Added boolean variable to review function, to prevent invalid input in review.html

--- reviews/views.py
+++ reviews/views.py
@@ -6,10 +6,18 @@ from django.shortcuts import render
 def review(request):
     if request.method == "POST": # If the form has been submitted...
         entered_username = request.POST['username'] # Get the username from the POST request
+        
+        if entered_username == "":
+            return render(request, 'reviews/review.html', { # Render the review page with an error message
+                'error': True
+            })
         print(entered_username)
         return HttpResponseRedirect('/thank-you') # Redirect to thank-you page
-    return render(request, 'reviews/review.html') # Render the review page
+    
+    return render(request, 'reviews/review.html', {
+        'error': False 
+    }) # Render the review page
 
 
 def thank_you(request):
-    return render(request, 'reviews/thank_you.html')
+    return render(request, 'reviews/thank_you.html') # Render the thank-you page
