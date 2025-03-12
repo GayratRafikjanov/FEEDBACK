06dfaf7c2a0603d817cd1c92bc9a9d10aa183943
GRafikjanov <gayratrafikjanov@gmail.com>
2025-03-12 15:50:33 +0500

    Added created app reviews to INSTALLED_APPS on settings.py

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.review, name='views_page'),
-    
+    path('', views.review),
+    path('thank-you', views.thank_you)
 ]
