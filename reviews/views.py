305f4e02770481d56f05edf08fb871aa767ba7c9
GayratRafikjanov <gayratrafikjanov@gmail.com>
2025-03-17 19:15:16 +0500

    added logic function to post and procces data

--- reviews/views.py
+++ reviews/views.py
@@ -1,21 +1,27 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import ReviewForm
+from .models import Review
 # Create your views here.
 
 def review(request):
     if request.method == "POST": # If the form has been submitted...
-        entered_username = request.POST['username'] # Get the username from the POST request
+        form = ReviewForm(request.POST) # A form bound to the POST data
         
-        if entered_username == "":
-            return render(request, 'reviews/review.html', { # Render the review page with an error message
-                'error': True
-            })
-        print(entered_username)
-        return HttpResponseRedirect('/thank-you') # Redirect to thank-you page
-    
+        if form.is_valid():
+            review = Review(
+              user_name=form.cleaned_data['user_name'], # Get the user_name from the form
+              review_text=form.cleaned_data['review_text'], # Get the review_text from the form
+              rating=form.cleaned_data['rating'], # Get the rating from the form
+           )
+            review.save() # Save the review to the database
+            return HttpResponseRedirect('/thank-you') # Redirect to thank-you page
+    else:
+        form = ReviewForm() # An unbound form
+                            # Render the review page with the form
+
     return render(request, 'reviews/review.html', {
-        'error': False 
+        'form': form # Pass the form object to the template
     }) # Render the review page
 
 
