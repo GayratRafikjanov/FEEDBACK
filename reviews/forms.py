3163e618b49d6f6b00b5ff91f65572e7cdfdec9b
GayratRafikjanov <gayratrafikjanov@gmail.com>
2025-03-17 19:17:06 +0500

    Nothing changed

--- reviews/forms.py
+++ reviews/forms.py
@@ -2,5 +2,10 @@ from django import forms
 
 
 class ReviewForm(forms.Form):
-    pass
+    user_name = forms.CharField( label="Your Name", max_length=100, error_messages={
+        "required": "Your name must not be empty",
+        "max_length": "Please enter a name no longer than 100 characters!"
+    })
 
+    review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea,max_length=100) # Add a review_text field to the form
+    rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5) # Add a rating field to the form
