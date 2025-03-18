0078b10421089ee9e9399c6a80c51b62af750a1f
GayratRafikjanov <gayratrafikjanov@gmail.com>
2025-03-18 15:52:44 +0500

    Added CBV and get, post functions

--- reviews/views.py
+++ reviews/views.py
@@ -2,28 +2,29 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import ReviewForm
 from .models import Review
+from django.views import View  # Import the View class from django.views
 # Create your views here.
 
-def review(request):
-    if request.method == "POST": # If the form has been submitted...
-        form = ReviewForm(request.POST) # A form bound to the POST data
-        
+class ReviewView(View):
+    
+    def get(self, request): # Define a get method
+        form = ReviewForm() # Create a form instance
+        return render(request, "reviews/review.html", { # Render the review.html template
+            "form": form # Pass the form to the template
+        })
+    
+    
+    def post(self, request):
+        form = ReviewForm(request.POST)
+
         if form.is_valid():
-            review = Review(
-              user_name=form.cleaned_data['user_name'], # Get the user_name from the form
-              review_text=form.cleaned_data['review_text'], # Get the review_text from the form
-              rating=form.cleaned_data['rating'], # Get the rating from the form
-           )
-            review.save() # Save the review to the database
-            return HttpResponseRedirect('/thank-you') # Redirect to thank-you page
-    else:
-        form = ReviewForm() # An unbound form
-                            # Render the review page with the form
-
-    return render(request, 'reviews/review.html', {
-        'form': form # Pass the form object to the template
-    }) # Render the review page
-
+            form.save()
+            return HttpResponseRedirect('/thank-you')
+        
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
+    
 
 def thank_you(request):
     return render(request, 'reviews/thank_you.html') # Render the thank-you page
