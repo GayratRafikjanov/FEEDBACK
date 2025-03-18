407f4fcd2ab4009464d1cf168b51b4736187bd24
GayratRafikjanov <gayratrafikjanov@gmail.com>
2025-03-18 15:49:32 +0500

    Added model in models.py

--- reviews/models.py
+++ reviews/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 class Review(models.Model):
     user_name = models.CharField(max_length=100)
-    review_text = models.TextField()
-    rating = models.IntegerField()
+    review_text = models.TextField(max_length=101)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
