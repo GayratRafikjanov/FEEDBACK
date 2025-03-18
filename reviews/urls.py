804c4f4e6630fe9ad87d7d34cba3d01331cb1517
GayratRafikjanov <gayratrafikjanov@gmail.com>
2025-03-18 15:53:24 +0500

    Configured URL path to CBV view

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.review),
+    path('', views.ReviewView.as_view()), # Change the path to an empty string
     path('thank-you', views.thank_you)
 ]
